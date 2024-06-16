b94a71cc5372cf0361b83290d6f09663d64dec2f
notronu <ya.sasha-mochalov1@yandex.ru>
2024-06-16 14:02:55 +0400

    Параллельные стримы

--- src/main/java/hogwarts/testhogwarts/controller/InfoController.java
+++ src/main/java/hogwarts/testhogwarts/controller/InfoController.java
@@ -1,6 +1,7 @@
 package hogwarts.testhogwarts.controller;
 
 
+import org.apache.commons.lang3.time.StopWatch;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 @RestController
@@ -31,6 +33,14 @@ public class InfoController {
         int sum = Stream.iterate(1, a -> a +1) .limit(1_000_000) .reduce(0, (a, b) -> a + b );
         var end = System.currentTimeMillis() - start;
         logger.info("Elapsed time: {} ms",end);
+        return sum;
+    }
+
+    @GetMapping("/sum2")
+    public int sum2() {
+        var sw = StopWatch.createStarted();
+        int sum = IntStream.iterate(1, a -> a +1).limit(1_000_000).sum();
+        logger.info("Elapsed time: {}", sw.elapsed());
         return sum;
     }
 }
